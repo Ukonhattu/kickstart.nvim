d32d350d4cd78811d875e2c8c4b84998c8e49109
Daniel Koch <danielkoch@fastmail.com>
2024-01-09 18:50:21 +0200

    Make the dashboard god into ascii art for better compatibility and because it kept breaking, keep the art in repo tho in case I want to still try to use it

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -3,7 +3,7 @@ return {
   event = "VimEnter",
   config = function()
     local alpha = require("alpha")
-    local alpha_term = require("alpha.term")
+ --[[    local alpha_term = require("alpha.term") ]]
     local dashboard = require("alpha.themes.dashboard")
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -23,16 +23,33 @@ return {
     dashboard.section.header.opts.hl = "AlphaShortcut"
     dashboard.section.buttons.opts.hl = "AlphaButtons"
 
-    local width = math.floor((vim.api.nvim_win_get_width(0) / 2) + 23)
-    local height = 26 -- two pixels per vertical space
-    dashboard.section.terminal.command = "cat | " .. os.getenv("HOME") .. "/.config/nvim/art/thisisfine.sh"
-    dashboard.section.terminal.width = width
-    dashboard.section.terminal.height = height
-    dashboard.section.terminal.opts.window_config = { border = 'none', col = (vim.api.nvim_win_get_width(0) * 2) }
-    dashboard.section.terminal.opts.redraw = true
+    -- local width = math.floor((vim.api.nvim_win_get_width(0) / 2) + 23)
+    -- local height = 26 -- two pixels per vertical space
+    -- dashboard.section.terminal.command = "cat | " .. os.getenv("HOME") .. "/.config/nvim/art/thisisfine.sh"
+    -- dashboard.section.terminal.width = width
+    -- dashboard.section.terminal.height = height
+    -- dashboard.section.terminal.opts.window_config = { border = 'none', col = (vim.api.nvim_win_get_width(0) * 2) }
+    -- dashboard.section.terminal.opts.redraw = true
 
 --    dashboard.section.header.val = "  𝕟 𝕖 𝕠 𝕧 𝕚 𝕞  "
       dashboard.section.header.val ={
+
+"             ``",
+"          	  ```",
+"               `    ``                `",
+"                ``  _ `      `       ``",
+"               `   |_| `  `` ``    `  `",
+"              ``  -___-_` `   `` ``   ``",
+"            ``   /      )      ``      `",
+"           `____/| (0) (0)_()    ``     ``",
+"          /|   | |   ^____)      ``      ``",
+"          ||   |_|    \\_//     Uɔ````   `` ``",
+"          ||    || |    |    ========`  ``  ``",
+"          ||    || |    |      ||     ``   `",
+"          ||     \\_\\   |\\     ||   ```    `",
+"          =========||====||    ||  ``       `",
+"            || ||   \\Ɔ || \\Ɔ   ||   ``    ``",
+"            || ||      ||      ||  `     ``",
 
 " _____   __ _  ___  ___    ___  ___   __   __  ___   __  __",
 "|_   _| | || ||_ _|/ __|  |_ _|/ __|  \\ \\ / / |_ _| |  \\/  | ",
@@ -41,9 +58,9 @@ return {
 }
                                                              
      dashboard.config.layout = {
-      { type = "padding", val = 1 },
-      dashboard.section.terminal,
-      { type = "padding", val = 2 },
+      { type = "padding", val = 5 },
+      -- dashboard.section.terminal,
+      -- { type = "padding", val = 2 },
       dashboard.section.header,
       { type = "padding", val = 2 },
       dashboard.section.buttons,
